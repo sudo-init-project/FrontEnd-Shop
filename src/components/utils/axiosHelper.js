610a5eecbfd01ae464c1ab0f1fe20a4dad54b2e7
facundoh3 <facundoh69@gmail.com>
2025-06-22 18:18:28 -0300

    Vamos a ver si funciona si dejo una URL estatica

--- src/components/utils/axiosHelper.js
+++ src/components/utils/axiosHelper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = ""
+axios.defaults.baseURL = "http://localhost:3000"
 axios.defaults.headers.post["Content-Type"] = 'application/json'
 
 export const getAuthToken = () =>{
